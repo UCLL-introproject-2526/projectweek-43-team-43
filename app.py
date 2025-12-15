b3b279eed9bca02b00d61eb719800c32b7ce223a
Job Maes <Job.Maes@ucll.begit config --global user.email Job.Maes@ucll.be>
2025-12-15 15:58:27 +0100

    windowtest

---
+++ app.py
@@ -0,0 +1,9 @@
+import pygame
+pygame.init()
+def create_main_surface(screen_size):
+    
+    pygame.display.set_mode(screen_size)
+    while True:
+        pass
+screen_size = (1024, 768)
+create_main_surface(screen_size)
