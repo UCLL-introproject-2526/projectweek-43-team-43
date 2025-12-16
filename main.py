b2cfb8c6afff1496cf7b502054a6fd62d2b2f618
Jari De Gelas <jaridegelas06@gmail.com>
2025-12-16 13:55:21 +0100

    basis main.py

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
 import pygame
+pygame.init()
 
 
+
+#scherm
+SCREEN_WIDTH = 1024
+SCREEN_HEIGHT = 768
+SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Hier Moet Onze Game Naam Komen")    
+
+
+
+
+
+#main loop
+
+running = True
+while running:
+    pygame.time.delay(10)
+
+
+    for event in pygame.event.get():
+       if event.type == pygame.QUIT:
+           running = False
+
+pygame.quit()
