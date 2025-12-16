2df6db18f42ec6b2e2eb298b726ae2d063c0f662
Jari De Gelas <jaridegelas06@gmail.com>
2025-12-16 14:37:05 +0100

    audio folder + muziek

---
+++ audio.py
@@ -0,0 +1,31 @@
+import pygame
+pygame.init()
+pygame.mixer.init()
+
+
+#scherm
+SCREEN_WIDTH = 1024
+SCREEN_HEIGHT = 768
+SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Hier Moet Onze Game Naam Komen")    
+
+
+
+
+
+#main loop
+
+running = True
+while running:
+    pygame.time.delay(10)
+
+
+    for event in pygame.event.get():
+       if event.type == pygame.QUIT:
+           running = False
+
+pygame.quit()
+
+
+#pygame.mixer.music.pause()
+#pygame.mixer.music.unpause()
