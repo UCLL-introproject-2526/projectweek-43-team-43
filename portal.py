fdfbcb6e99ac168d497b6a0ad2d89691b3857c41
Job Maes <Job.Maes@ucll.begit config --global user.email Job.Maes@ucll.be>
2025-12-17 10:27:14 +0100

    portal

--- portal.py
+++ portal.py
@@ -10,12 +10,13 @@ RED = (255, 0, 0)
 WHITE = (255, 255, 255)
 BLUE = (0, 100, 255)
 GREEN = (0, 255, 0)
+PURPLE = (138, 43, 226)
 
 SCREEN_SIZE = (1024, 768)
-BLOCK_COUNT = 30
+BLOCK_COUNT = 1
 PLAYER_RADIUS = 20
 
-MOVEMENT_SPEED = 2
+MOVEMENT_SPEED = 7
 START_SPEED = 3
 SPEED_INCREASE = 0.002
 MAX_SPEED = 12
@@ -40,9 +41,14 @@ def create_blocks():
 def update_blocks(blocks, fall_speed):
     for block in blocks:
         block[1] += fall_speed
-        if block[1] > SCREEN_SIZE[1]:
+        if fall_speed > 0 and block[1] > SCREEN_SIZE[1]:
             block[2] = random.randint(20, 60)
             block[1] = random.randint(-150, 0)
+            block[0] = random.randint(0, SCREEN_SIZE[0] - block[2])
+        
+        elif fall_speed < 0 and block[1] < -block[2]:
+            block[2] = random.randint(20, 60)
+            block[1] = random.randint(SCREEN_SIZE[1], SCREEN_SIZE[1] + 150)
             block[0] = random.randint(0, SCREEN_SIZE[0] - block[2])
 
 def show_game_over(surface, final_score):
@@ -76,9 +82,12 @@ def show_game_over(surface, final_score):
                 if event.key == pygame.K_SPACE:
                     waiting = False
 
-def render_frame(surface, blocks, player_x, player_y, current_score, heart_img, current_lives, immunity_timer, background_img, img_small, img_medium, img_large):
+def render_frame(surface, blocks, player_x, player_y, current_score, heart_img, current_lives, immunity_timer, background_img, img_small, img_medium, img_large, portal_rect=None, portal_img=None):
     surface.fill(BLACK)
     surface.blit(background_img, (0,0))
+
+    if portal_rect and portal_img:
+        surface.blit(portal_img, (portal_rect.x, portal_rect.y))
     
    
     for block in blocks:
@@ -129,6 +138,9 @@ def main():
     heart_image = pygame.image.load("images/lives.png").convert_alpha()
     heart_image = pygame.transform.scale(heart_image, (30, 30))
 
+    portal_img = pygame.image.load("images/portal.png").convert_alpha()
+    portal_img = pygame.transform.scale(portal_img, (200, 40))
+
     
     x = SCREEN_SIZE[0] // 2
     y = SCREEN_SIZE[1] - 100
@@ -139,6 +151,9 @@ def main():
     score = 0
     lives = 3
     immunity_timer = 0
+
+    level_flipped = False
+    portal_active = False
 
     running = True
     while running:
@@ -175,21 +190,72 @@ def main():
         x += x_velocity
         y += y_velocity
         score += 1 
-        fall_speed = min(fall_speed + SPEED_INCREASE, MAX_SPEED)
+        if not level_flipped:
+            fall_speed = min(fall_speed + SPEED_INCREASE, MAX_SPEED)
+        else:
+            fall_speed = max(fall_speed - SPEED_INCREASE, -MAX_SPEED)
         update_blocks(blocks, fall_speed)
 
       
         if x < PLAYER_RADIUS: x = PLAYER_RADIUS
         if x > SCREEN_SIZE[0] - PLAYER_RADIUS: x = SCREEN_SIZE[0] - PLAYER_RADIUS
+        if y < PLAYER_RADIUS: y = PLAYER_RADIUS
+        if y > SCREEN_SIZE[1] - PLAYER_RADIUS: y = SCREEN_SIZE[1] - PLAYER_RADIUS
 
         player_rect = pygame.Rect(x - PLAYER_RADIUS, y - PLAYER_RADIUS, PLAYER_RADIUS * 2, PLAYER_RADIUS * 2)
+
+        portal_rect = None
+        if score // 10 >= 100 and not level_flipped:
+            portal_active = True
+            
+            portal_rect = pygame.Rect(SCREEN_SIZE[0]//2 - 100, 20, 200, 40)
+            portal_center = (portal_rect.centerx, portal_rect.centery)
+
+            dist_x = portal_center[0] - x
+            dist_y = portal_center[1] - y
+            
+            x += dist_x * 0.05
+            y += dist_y * 0.05
+
+            if player_rect.colliderect(portal_rect):
+                for i in range(40):
+                   
+                    render_frame(surface, blocks, x, y, score, heart_image, lives, 0, background_image, meteor_small, meteor_medium, meteor_large, portal_rect, portal_img)
+                    
+                   
+                    x += (portal_center[0] - x) * 0.2
+                    y += (portal_center[1] - y) * 0.2
+                    
+                    radius = int(PLAYER_RADIUS * (1 - i/40))
+                    if radius > 0:
+                        pygame.draw.circle(surface, BLUE, (int(x), int(y)), radius)
+                    
+                    pygame.display.flip()
+                    clock.tick(60)
+
+                
+                level_flipped = True
+                portal_active = False
+                fall_speed = -fall_speed 
+                x, y = SCREEN_SIZE[0]//2, 100 
+                
+                blocks = [] 
+                for _ in range(BLOCK_COUNT):
+                    size = random.randint(20, 60)
+                    bx = random.randint(0, SCREEN_SIZE[0] - size)                    
+                    by = random.randint(SCREEN_SIZE[1], SCREEN_SIZE[1] + 500)
+                    blocks.append([bx, by, size])
+                    
+                continue 
+            
+
         
         for block in blocks:
             block_rect = pygame.Rect(block[0], block[1], block[2], block[2])
             
             if player_rect.colliderect(block_rect) and immunity_timer == 0:                
                 lives -= 1
-                render_frame(surface, blocks, x, y, score, heart_image, lives, 1, background_image, meteor_small, meteor_medium, meteor_large)
+                render_frame(surface, blocks, x, y, score, heart_image, lives, 1, background_image, meteor_small, meteor_medium, meteor_large, portal_rect, portal_img)
                 pygame.time.delay(300)
                 immunity_timer = 90
                 
@@ -198,7 +264,7 @@ def main():
                     lives = 3  
                     score = 0
                     fall_speed = START_SPEED
-              
+                    level_flipped = False
                     x = SCREEN_SIZE[0] // 2
                     y = SCREEN_SIZE[1] - 100
                     x_velocity = 0
@@ -206,7 +272,7 @@ def main():
                 break 
 
         
-        render_frame(surface, blocks, x, y, score, heart_image, lives, immunity_timer, background_image, meteor_small, meteor_medium, meteor_large)
+        render_frame(surface, blocks, x, y, score, heart_image, lives, immunity_timer, background_image, meteor_small, meteor_medium, meteor_large, portal_rect, portal_img)
 
     pygame.quit()
     sys.exit()
